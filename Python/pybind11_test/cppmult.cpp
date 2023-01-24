e82998855aae66dcf9d88210ed8baf53da8faac9
swordliongit <kilicarslan.business@gmail.com>
2023-01-24 17:53:25 +0300

    modified:   .vscode/c_cpp_properties.json 	new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	new file:   Python/pybind11_test/CMakeLists.txt 	deleted:    Python/pybind11_test/cppmult.cpp 	new file:   Python/pybind11_test/libcppmult.so 	new file:   Python/pybind11_test/main.cpp 	deleted:    Python/pybind11_test/pybind11_wrapper.cpp 	new file:   Python/pybind11_test/rooster.exe 	modified:   Python/pybind11_test/tasks.py 	new file:   Python/pybind11_test/test.cpp

--- Python/pybind11_test/cppmult.cpp
+++
@@ -1,6 +0,0 @@
-
-
-float cppmult(float arg1, float arg2)
-{
-    return arg1 + arg2;
-}
---
+++ Python/pybind11_test/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+float cppmult(float arg1, float arg2)
+{
+    return arg1 + arg2;
+}
+
+int main()
+{
+    cout << cppmult(2, 3);
+}
